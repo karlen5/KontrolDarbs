d818e4fe07a7342ec119efe793423e5cf7957f66
DeivisStasuls <deivis.stasuls@gmail.com>
2026-03-31 13:33:02 +0300

    Lietotājs var apskatīt vienu grāmatu ar visiem parametriem.

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,7 +4,7 @@
     <input type="submit" value="Dzēst">
 </form>
 
-<h2>{{ title }}</h2>
-<h3>{{ author }}</h3>
-<p>{{ released_at }}</p>
+<h2>{{ $singleBook->title }}</h2>
+<h3>{{ $singleBook->author }}</h3>
+<p>{{ $singleBook->released_at }}</p>
 <a href="/books">All books</a>
